d75fc2917f7e30fbcf494a759ccda3a980f55a40
Pereslavtsev Vladimir <vovapereslavtsev@gmail.com>
2025-02-21 09:30:24 +0300

    routing added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signin from "./pages/Signin";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/dashboard" element={[<Header />, <Dashboard />]} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
